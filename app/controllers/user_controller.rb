b021188b45ea2ef4f9b5af32d52cc0308f5e9f48
ASHICK SHERIFF <ashicksheriff@HAT-ADMINs-MacBook-Pro.local>
2023-08-01 09:48:30 +0530

    update1

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,26 @@
 class UserController < ApplicationController
-    def index
-    end
-  
+    def user_index
+        @user_data = {} # Initialize an empty hash to hold the user details
+      end
+    
     def next
       # Handle the form submission and processing for Form 1
       redirect_to address_path
     end
   
     def cancel
-      redirect_to root_path
-    end
+        @user_data = {} # Reset the user data when the user cancels
+        redirect_to root_path
+      end
+
+    def user_params
+        permitted_params = %i[
+          first_name last_name email phone_number gender date_of_birth tenth_mark twelfth_mark
+          school_name college_degree college_name cgpa father_name mother_name
+          father_mobile_number mother_mobile_number height weight religion community
+          marital_status facebook_account instagram_account twitter_account
+        ]
+        params.require(:user_details).permit(permitted_params)
+      end
+
   end
